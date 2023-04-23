a4ddd87f085fd58aab5dba6d9fcc88d5ccbba4d5
radiostatic0 <yayaroses102@yahoo.com>
2023-04-23 19:27:12 -0400

    Renamed the old main.c to main_old.c; made new main from scratch that draws one horizontal line 1 pixel wide on the screen on an orange background. (Compiles and tested on dc via serial)

--- attempt/main.c
+++ attempt/main.c
@@ -1,167 +1,97 @@
-
 #include <kos.h>
 
 #include <plx/matrix.h>
 #include <plx/prim.h>
 
-#include "vmu_img.h"
-
-#include <stdbool.h>
-
-//initialize KOS
 KOS_INIT_FLAGS(INIT_DEFAULT);
 
-//initialize romdisk
-extern uint8 romdisk[];
-KOS_INIT_ROMDISK(romdisk);
-
-
-bool exitProgram = false;
-
-pvr_poly_hdr_t nontexturedHeader;
-
-vector_t verts[4] = {
-	{ -1, 1, 0, 1},
-	{ -1, -1, 0, 1},
-	{ 1, 1, 0, 1},
-	{ 1, -1, 0, 1}
-};
-
-//pvr_ptr_t smile;
-
 void init(){
-
-	//init VMU image
-	maple_device_t *vmu = maple_enum_type(0, MAPLE_FUNC_LCD);
-	vmu_draw_lcd(vmu, vmu_carl);
-
-	//init pvr (PowerVR graphics chip)
 	pvr_init_defaults();
-	plx_mat3d_init(); //init the Parralax library
 
-	// compile a polygon header (information about the polygon, ie
-	// whether it should be textured) before giving it your vertices
-	pvr_poly_cxt_t nontexturedContext; //like a human readable version of the header
-
-	// setup the context, giving it a pointer to put the data and the poly list type
-	pvr_poly_cxt_col(&nontexturedContext, PVR_LIST_OP_POLY);
-
-	nontexturedContext.gen.culling = PVR_CULLING_CW;
-
-	pvr_poly_compile(&nontexturedHeader, &nontexturedContext);
-	// ^ turns it from the human readable context to the real header for the graphics chip
-
-	pvr_set_bg_color(1.0, 0.5, 0.2);
-
-	//setup the camera
-	plx_mat3d_mode(PLX_MAT_PROJECTION); //projection matrix
-	plx_mat3d_identity(); //resetting it back to 0 (no transformation)
-
-	plx_mat3d_perspective(60.0f, 640.0f / 480.0f, 0.1f, 100.0f); // camera "lens"
-	// field of view, aspect ratio, near plane, far plane (depth of field)
-
-	//actually position camera
-	plx_mat3d_mode(PLX_MAT_MODELVIEW);
-	plx_mat3d_identity();
-
-	point_t cameraPosition = { 0.0f, 0.0f, 5.0f, 1.0f };
-	// with this setup:
-	// 0,0 is center of screen
-	// pos Y is up, pos X is right, pos Z is out of screen
-
-	//positioned at 0,0 , 5 units away
-	//last value indicates that this is describing a point and not a direction
-
-	
-	point_t cameraTarget = { 0.0f, 0.0f, 0.0f, 1.0f }; //which point the camera should look at
-
-	vector_t cameraUp = { 0.0f, 1.0f, 0.0f, 0.0f }; //positive Y is "up"
-
-	plx_mat3d_lookat(&cameraPosition, &cameraTarget, &cameraUp);
-
-	//smile = pvr_mem_malloc(25 * 25 * 2);
-	//png_to_texture("/rd/smile.png", smile, PNG_NO_ALPHA);
+	pvr_set_bg_color(1.0,0.5,0.2);
 }
 
-void update(){
-	plx_mat_identity();
-	plx_mat3d_apply_all(); //apply camera settings
+void draw_horizontal_line(float x1, float x2, float y, uint8 a, uint8 r, uint8 g, uint8 b){
+	// Draws a 1-pixel wide horziontal line from x1 to x2 at height y
+	// with color determined by rgba
 
-	vector_t transformedVerts[4];
-	plx_mat_transform(verts, transformedVerts, 4, 4*sizeof(float));
-	// input: verts, output: transformedVerts, # of verts, size of each vert
+	pvr_poly_hdr_t hdr;
+	pvr_poly_cxt_t cxt;
+	pvr_vertex_t vert;
 
+	// make sure x2 is larger than x1
+	float swap_x;
+	if (x1 > x2) {
+		swap_x = x1;
+		x1 = x2;
+		x2 = swap_x;
+	}
+	pvr_poly_cxt_col(&cxt, PVR_LIST_TR_POLY);
+	pvr_poly_compile(&hdr, &cxt);
 
-	// wait for the pvr to accept a frame
-	pvr_wait_ready();
+	pvr_prim(&hdr, sizeof(hdr));
 
-	pvr_scene_begin(); //start of one frame
+	vert.flags = PVR_CMD_VERTEX;
+	vert.x=x1;
+	vert.y=y+1;
+	vert.z=5.0f;
+	vert.u=0;
+	vert.v=0;
+	vert.argb=PVR_PACK_COLOR(a/255, r/255, g/255, b/255);
+	vert.oargb=0;
+	pvr_prim(&vert, sizeof(vert));
 
-	pvr_list_begin(PVR_LIST_OP_POLY); //opaque polygon mode
+	vert.y=y;
+	pvr_prim(&vert, sizeof(vert));
 
-	pvr_prim(&nontexturedHeader, sizeof(pvr_poly_hdr_t)); // send header to pvr chip to tell it how we're gonna draw it...
-	//now let's tell it what to draw
+	vert.x=x2;
+	vert.y=y+1;
+	pvr_prim(&vert, sizeof(vert));
 
-	
-	// triangle
-	/*
-	plx_vert_fnp(PVR_CMD_VERTEX, -1, 1, 0, 1, 1, 1, 1); //top left, white
-	// x, y, z, alpha, r, g, b
-	plx_vert_fnp(PVR_CMD_VERTEX, -1, -1, 0, 1, 1, 0, 0); //bottom left, red
+	vert.flags=PVR_CMD_VERTEX_EOL;
+	vert.y = y;
+	pvr_prim(&vert, sizeof(vert));
+}
 
-	plx_vert_fnp(PVR_CMD_VERTEX, 1, 1, 0, 1, 0, 1, 1); //top right, green
-	// second triangle
-	plx_vert_fnp(PVR_CMD_VERTEX_EOL, 1, -1, 0, 1, 0, 0, 1); //bottom right, blue
-	// ^ EOL tells it that it's the last vertex in the list
-	*/
+void draw_frame(){
 
-	plx_vert_fnp(PVR_CMD_VERTEX, transformedVerts[0].x, transformedVerts[0].y, transformedVerts[0].z, 1, 1, 1, 1); //top left, white
-	// x, y, z, alpha, r, g, b
-	plx_vert_fnp(PVR_CMD_VERTEX, transformedVerts[1].x, transformedVerts[1].y, transformedVerts[1].z, 1, 1, 0, 0); //bottom left, red
+	pvr_scene_begin();
 
-	plx_vert_fnp(PVR_CMD_VERTEX, transformedVerts[2].x, transformedVerts[2].y, transformedVerts[2].z, 1, 0, 1, 1); //top right, green
-	// second triangle
-	plx_vert_fnp(PVR_CMD_VERTEX_EOL, transformedVerts[3].x, transformedVerts[3].y, transformedVerts[3].z, 1, 0, 0, 1); //bottom right, blue
+	pvr_list_begin(PVR_LIST_OP_POLY);
+	//opaque drawing here
+	pvr_list_finish();
 
-
-
-
+	pvr_list_begin(PVR_LIST_TR_POLY);
+	//translucent drawing here
+	draw_horizontal_line(200.0,300.0,200.0,255,255,0,255);
 
 	pvr_list_finish();
 
-	pvr_scene_finish(); //end of one frame
+	pvr_scene_finish();
 }
 
-void cleanup(){
-	maple_device_t *vmu = maple_enum_type(0, MAPLE_FUNC_LCD);
-	vmu_draw_lcd(vmu, vmu_clear);
 
-	pvr_shutdown();
-}
+int main(){
 
-int main(void){
+	int exitProgram = 0;
 
 	init();
 
-	
-	while (!exitProgram){
+	printf("Hello world!\n");
+
+	while(!exitProgram){
 		maple_device_t *controller = maple_enum_type(0, MAPLE_FUNC_CONTROLLER);
 		cont_state_t *controllerState = (cont_state_t*) maple_dev_status(controller);
 
 		//check controller for START button press
 		
 		if (controllerState->buttons & CONT_START){
-			exitProgram = true;
+			exitProgram = 1;
 		}
 		
-
-		//MAPLE_FOREACH_BEGIN(MAPLE_FUNC_CONTROLLER,)
-		update();
-		
+		draw_frame();
 	}
 
-	cleanup();
-
-	return 0;
+	pvr_shutdown();
 
 }
