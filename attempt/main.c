9183b5344b774255df403be243e06baf295505ea
radiostatic0 <yayaroses102@yahoo.com>
2023-04-23 21:20:31 -0400

    Now it draws 4 lines on an orange background, each line is a different color

--- attempt/main.c
+++ attempt/main.c
@@ -1,7 +1,15 @@
+//https://dreamcast.wiki/PowerVR_Introduction
+
+// resolution is 640w x 480h
+
+
 #include <kos.h>
 
 #include <plx/matrix.h>
 #include <plx/prim.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
 KOS_INIT_FLAGS(INIT_DEFAULT);
 
@@ -10,6 +18,78 @@ void init(){
 
 	pvr_set_bg_color(1.0,0.5,0.2);
 }
+
+void draw_triangle(float x1, float y1,
+				   float x2, float y2,
+				   float x3, float y3,
+				   uint8 a, uint8 r, uint8 g, uint8 b)
+				   {
+	// POINTS SUBMITTED MUST BE IN CLOCKWISE ORDER
+	pvr_poly_hdr_t hdr;
+	pvr_poly_cxt_t cxt;
+	pvr_vertex_t vert;
+
+	pvr_poly_cxt_col(&cxt, PVR_LIST_TR_POLY);
+	pvr_poly_compile(&hdr, &cxt);
+	pvr_prim(&hdr, sizeof(hdr));
+
+	vert.flags = PVR_CMD_VERTEX;
+	vert.x = x1;
+	vert.y = y1;
+	vert.z = 5.0f;
+	vert.u = 0;
+	vert.v = 0;
+	vert.argb = PVR_PACK_COLOR(a/255, r/255, g/255, b/255);
+	vert.oargb = 0;
+	pvr_prim(&vert, sizeof(vert));
+
+	vert.x = x2;
+	vert.y = y2;
+	pvr_prim(&vert, sizeof(vert));
+
+	vert.flags = PVR_CMD_VERTEX_EOL; //tells PVR that this will be the final point
+	vert.x = x3;
+	vert.y = y3;
+	pvr_prim(&vert, sizeof(vert));
+}
+
+void draw_vertical_line(float x, float y1, float y2, uint8 a, uint8 r, uint8 g, uint8 b) {
+	pvr_poly_hdr_t hdr;
+	pvr_poly_cxt_t cxt;
+	pvr_vertex_t vert;
+	float swap_y;
+	if(y1>y2) {
+		swap_y = y1;
+		y1 = y2;
+		y2 = swap_y;
+	}
+
+	pvr_poly_cxt_col(&cxt, PVR_LIST_TR_POLY);
+	pvr_poly_compile(&hdr, &cxt);
+
+	pvr_prim(&hdr, sizeof(hdr));
+	vert.flags = PVR_CMD_VERTEX;
+	vert.x = x;
+	vert.y = y2;
+	vert.z = 5.0f;
+	vert.u = 0;
+	vert.v = 0;
+	vert.argb = PVR_PACK_COLOR(a/255, r/255, g/255, b/255);
+	vert.oargb = 0;
+	pvr_prim(&vert, sizeof(vert));
+
+	vert.y = y1;
+	pvr_prim(&vert, sizeof(vert));
+
+	vert.x = x+1;
+	vert.y = y2;
+	pvr_prim(&vert, sizeof(vert));
+
+	vert.flags = PVR_CMD_VERTEX_EOL;
+	vert.y = y1;
+	pvr_prim(&vert, sizeof(vert));
+}
+
 
 void draw_horizontal_line(float x1, float x2, float y, uint8 a, uint8 r, uint8 g, uint8 b){
 	// Draws a 1-pixel wide horziontal line from x1 to x2 at height y
@@ -63,7 +143,11 @@ void draw_frame(){
 
 	pvr_list_begin(PVR_LIST_TR_POLY);
 	//translucent drawing here
-	draw_horizontal_line(200.0,300.0,200.0,255,255,0,255);
+	
+	draw_horizontal_line(100, SCREEN_WIDTH-100, 100, 255, 255, 0, 0);
+	draw_vertical_line(SCREEN_WIDTH-100, 100, SCREEN_HEIGHT-100, 255, 0, 255, 0);
+	draw_horizontal_line(100, SCREEN_WIDTH-100, SCREEN_HEIGHT-100, 255, 0, 0, 255);
+	draw_vertical_line(100, SCREEN_HEIGHT-100, 100, 255, 255, 255, 255);
 
 	pvr_list_finish();
 
@@ -78,6 +162,7 @@ int main(){
 	init();
 
 	printf("Hello world!\n");
+	printf("How are you today? :)\n");
 
 	while(!exitProgram){
 		maple_device_t *controller = maple_enum_type(0, MAPLE_FUNC_CONTROLLER);
